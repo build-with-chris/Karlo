a113059faaead94dc423acac56be86b995344f4e
Chris <chris@MacBook-Air-von-Chris.local>
2026-01-10 11:13:19 +0100

    Fix TypeScript-Fehler: Verwende typeof translations[Language] für flexiblen Typ

--- contexts/LanguageContext.tsx
+++ contexts/LanguageContext.tsx
@@ -3,7 +3,7 @@
 import React, { createContext, useContext, useState, useEffect } from "react";
 import { Language, translations, defaultLanguage } from "@/lib/i18n";
 
-type TranslationsType = typeof translations.de;
+type TranslationsType = typeof translations[Language];
 
 type LanguageContextType = {
   language: Language;
